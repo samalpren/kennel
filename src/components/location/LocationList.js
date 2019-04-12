a925b0ac6861a79761b278b521a1379eb17c54d8
Sam Alpren <samalpren1@gmail.com>
2019-04-12 09:46:57 -0500

    creating only to pull down to finish my other projects that are not committed

--- src/components/location/LocationList.js
+++ src/components/location/LocationList.js
@@ -1,19 +1,22 @@
 import React, { Component } from 'react';
 
 export default class LocationList extends Component {
-  render() {
-    return (
-      <React.Fragment>
-      <div>
+  render(){
+      return (
         <div className="locationWrapper">
-        <h1>Locations</h1>
-          <h4>Nashville North Location</h4>
-          <h5>500 Puppy Way</h5>
-          <h4>Nashville South Location</h4>
-          <h5>500 Kitty Way</h5>
+        <section className="locationCard">
+        <h1>Location List</h1>
+            {
+              this.props.locations.map(location =>
+                <div key={location.id}>
+                    <h4>{location.name}</h4>
+                    <p>{location.address}</p>
+                </div>
+              )
+            }
+        </section>
         </div>
-      </div>
-      </React.Fragment>
-    );
+
+      );
   }
 }
